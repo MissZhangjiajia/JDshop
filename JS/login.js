454a205eaaaba517339f7081c16769a87d9cca6c
zjj <zjj@163.com>
2019-01-12 22:01:29 +0800

    secondday

--- JS/login.js
+++ JS/login.js
@@ -1,5 +1,30 @@
-$(function(){
-	$(".tab a").click(function() {  
-	console.log("aa");
+$(function(){				
+	$(".tab a").click(function(e) { 
+		e.preventDefault();
+	$(".tab a").removeClass("tagstyle")
+	$(this).addClass("tagstyle");
+		
+		
    	});
+   	$(".tab a:first").click(function(){
+			$(".img1").stop().animate({"left":0},500);
+			$(".img2").show().css("right","-20px");
+		})
+   	$(".img").mouseenter(function(){  		$(".img1").stop().animate({"left":0},500);
+		$(".img2").show().css("right","-20px");
+   	}).mouseleave(function(){
+   		$(".img1").stop().animate({"left":"64px"},500);
+   		$(".img2").hide();
+   	})
+   		$(".tab a:first").click(function(){
+			$(".img1").show().stop().animate({"left":0},500);
+			$(".img2").show().css("right","-15px");
+			$(".login1").hide();
+		})
+   		$(".tab a:last").click(function(){
+   			$(".login1").css("display","block");
+   			$(".img1,.img2").hide();
+   			$(".login1").css("background","#fff")
+   		})
+   		
 })
